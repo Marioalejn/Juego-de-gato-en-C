024da7ef3d328e61d4a79a2c2746e09999e9a0fb
Mario Alvarez <152515406+Marioalejn@users.noreply.github.com>
2025-02-15 17:09:18 -0600

    Creación de arreglo 3x3 y guardar selección del jugador
    Primero se llena el arreglo con 2 for anidados y se agregan solo elementos vacíos, la función coordenadas hace que el jugador elija la coordenada donde quiere poner su ficha, si se sale del rango no guarda nada.
    
    Aún falta hacer la recursividad de la función para que entre hasta que alguien gane o empate.

--- main_code.c
+++ main_code.c
@@ -1,6 +1,48 @@
-inclide<stdio.h>
+#include <stdio.h>
+ 
+void coordenadas();
 
-int main(){
-//ejemplo para comprobar la rama 
-return 0;
+int main()
+{
+    char array[3][3];
+    int i,j;
+    //for que llena el arreglo con elementos vacíos
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            array[i][j] = '\0';
+        }
+    }
+    //Llamado a la función
+    coordenadas(array);
+    
+    return 0;
+}
+
+//Función para que el jugador eliga la coordenada 
+void coordenadas(char array[3][3]){
+    int fila, columna, player=1;
+    printf("Ingresa la fila: ");
+    scanf("%i", &fila);
+    printf("Ingresa la columna: ");
+    scanf("%i", &columna);
+    
+    if(fila >= 0 && fila <= 2 && columna >= 0 && columna <= 2){ //Si no está ocupada entra al if
+        if(player == 1 && array[fila][columna] == '\0'){
+            array[fila][columna] = 'X'; //jugador 1 es X y jugador 2 es O
+            player = 2; //Para que cambie de jugador y no se pongan siempre los mismos signos
+            printf("array[%i][%i]: %c", fila,columna,array[fila][columna]); //BORRARLOS, SOLO PARA VERIFICAR QUE SI GUARDARA DATOS
+        }
+        if(player == 2 && array[fila][columna] == '\0'){
+            array[fila][columna] = 'O';
+            player = 2;
+            printf("array[%i][%i]: %c", fila,columna,array[fila][columna]);//BORRARLOS, SOLO PARA VERIFICAR QUE SI GUARDARA DATOS
+        }
+    }
+    if(!(fila>=0 && fila<=2 && columna>=0 && columna<=2)){ //else por si agrega una coordenada ocupada o que no existe en el 0,1,2 del arreglo 3x3
+        printf("Coordenada seleccionada ocupada o erronea, ingresa otra...\n");
+        getchar();
+        coordenadas(array);
+    }
+    
+    //Tal vez tenga que retornar el jugador o algo
 }
